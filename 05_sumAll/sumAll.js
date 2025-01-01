fe8298ee34aff52ef62f9d61ef17764c3271ad47
judywsl <j542yu@uwaterloo.ca>
2025-01-01 15:34:44 -0500

    Complete exercise 05
    Note to self: don't forget about Number methods :)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(lowbound, highbound) {
+    if (lowbound > highbound) {
+        let tmp = lowbound;
+        lowbound = highbound;
+        highbound = tmp;
+    }
+    if (!Number.isInteger(lowbound) ||
+        !Number.isInteger(highbound) ||
+        (lowbound < 0) ||
+        (highbound < 0))
+    {
+        return 'ERROR';
+    } else {
+        let sum = 0;
+        for (let i = lowbound; i <= highbound; ++i) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
