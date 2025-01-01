1368d53db8c64ac8c4eec59c08fc3a5d0e239f2d
judywsl <j542yu@uwaterloo.ca>
2025-01-01 15:41:39 -0500

    Complete exercise 06
    Note to self: increase clarity of code by using descriptive boolean variables

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isDivisibleByFour = (year % 4 === 0);
+    let isCenturyYear = (year % 100 === 0);
+    let isDivisibleByFourHundred = (year % 400 === 0);
+    
+    if (isDivisibleByFour && 
+        (!isCenturyYear || isDivisibleByFourHundred)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
