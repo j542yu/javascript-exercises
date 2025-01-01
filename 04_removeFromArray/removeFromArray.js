63a4ff9114347811ab4c0e1c51ac5efcf1aba53e
judywsl <j542yu@uwaterloo.ca>
2025-01-01 15:18:26 -0500

    Complete exercise 04 with explanation of .filter() for self learning

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+// ...args uses spread syntax for multiple optional arguments
+const removeFromArray = function(array, ...args) {
+    /* 
+    (explaination to self for learning purposes)
+     - .filter() takes in a callbackFn which is executed for
+        each element in the array and element is removed if
+        callbackFn returns a falsy value 
+     - note use of arrow function
+     - if not in args, keep in array!
+    */
+    return array.filter((element) => !args.includes(element));
 };
 
 // Do not edit below this line
