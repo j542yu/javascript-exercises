77b1368da2ee5ceebcce6fda025b90855f9437c3
judywsl <j542yu@uwaterloo.ca>
2025-01-01 15:56:21 -0500

    Attempt 1 at exercise 07
    More steps than necessary in rounding process :(

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let result = (tempFahrenheit - 32) * 5 / 9;
+  return +roundDecimalPlaces(result, 1);
 };
 
-const convertToFahrenheit = function() {
+function roundDecimalPlaces (num, decimalPlaces) {
+  if (Math.floor(num) !== num) {
+    num = num.toFixed(decimalPlaces);
+  }
+  return +num;
+}
+
+const convertToFahrenheit = function(tempCelsius) {
+  let result = tempCelsius* 9 / 5 + 32;
+  return +roundDecimalPlaces(result, 1);
 };
 
 // Do not edit below this line
